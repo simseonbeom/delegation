e2039d41eed058e2c81d9de64e6ee63c8958ece4
kindtiger <seonbeom2@gmail.com>
2023-12-14 07:34:16 +0900

    template 수정

--- client/js/part02.js
+++ client/js/part02.js
@@ -1,16 +1,28 @@
 
 
 
-
-// 1. 너무 무거움
-// 2. pure javascript 잘 못 쓰는 사람이 별로 없음
-// 3. javascript가 jquery (ajax) 처럼 쉬워진다면? 
-// 4. Single Page Application 불가능 
-// fetch API  => Axios
-
-
-
-
+const data = [
+  {
+    "id": 1,
+    "src": "visual1.jpg",
+    "alt": "모던한 테이블과 화분의 조화를 표현한 공간"
+  },
+  {
+    "id": 2,
+    "src": "visual2.jpg",
+    "alt": "강렬한 의자의 색상과 따뜻한 느낌의 공간"
+  },
+  {
+    "id": 3,
+    "src": "visual3.jpg",
+    "alt": "호텔 라운지 느낌의 편안한 의자가 있는 공간"
+  },
+  {
+    "id": 4,
+    "src": "visual4.jpg",
+    "alt": "물방을 모양의 독특한 디자인의 의자들을 나열한 공간"
+  }
+]
 
 
 
--- client/js/part04.js
+++ client/js/part04.js
@@ -1,18 +1,3 @@
 
 
-// 누른 대상을 찾기
-// 누른 대상의 data-name 값 찾기
-
-
-
-//made by kindtiger
-
-
-
-
-
-
-
-
-
 
